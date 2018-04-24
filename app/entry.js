572a2416b18e0cc251ffd9258dde7aa05339582f
Sarah DeBey <sarah.debey@gmail.com>
2018-04-24 08:48:15 -0700

    project now building with webpack

---
+++ app/entry.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// const fs = require ('fs');
+//
+// function Fetch () {
+//   fetch('https://data.seattle.gov/resource/c4b9-qn5y.json')
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(myJson => {
+//       fs.writeFile('wages.js', myJson, err => {
+//         if (err) throw err;
+//         console.log('file written');
+//       })
+//       // console.log(myJson);
+//     });
+// }
+//
+// Fetch();
