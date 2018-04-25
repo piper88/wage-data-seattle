40484a1239e3f0f50838c4bc91de3f8c9c8c68c3
Sarah DeBey <sarah.debey@gmail.com>
2018-04-25 08:44:20 -0700

    server working, data being fetched and displayed in console

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-// 'use strict';
-//
-// const express = require('express');
-// const app = express();
-// 
-// const PORT = 3000;
-// const debug= require('debug')('wage-data-seattle:server');
-//
-// const server = module.exports = app.listen(PORT, () => {
-//   //not working
-//   debug(`server up on ${PORT}`);
-//   //working
-//   console.log(`server up on ${PORT}`);
-// });
-//
-// server.isRunning = true;
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+const debug= require('debug')('wage-data-seattle:server');
+
+app.use(express.static(`${__dirname}/build`));
+
+const server = module.exports = app.listen(PORT, () => {
+  //not working
+  debug(`server up on ${PORT}`);
+  //working
+  console.log(`server up on ${PORT}`);
+});
+
+server.isRunning = true;
